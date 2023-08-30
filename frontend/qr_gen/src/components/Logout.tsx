83c2a5c837d25d4501a0e1ce40749d025f55b21c
imdad Adelabou <imdadadelabou@imdads-MacBook-Pro.local>
2023-08-30 13:47:45 +0200

    [feat] implement logout to the dashboard

--- frontend/qr_gen/src/components/Logout.tsx
+++ frontend/qr_gen/src/components/Logout.tsx
@@ -1,10 +1,19 @@
 import { APP_MESSAGE } from "../helpers/constants";
 import style from "../style/logout.module.css";
 import LogoutIcon from "../assets/logoutIcon.svg";
+import { useContext } from "react";
+import { UserContext } from "../views/stores/UserContext";
+import { UserContextType, UserType } from "../helpers/types";
 
 function Logout() {
+  const { updateUser } = useContext(UserContext) as UserContextType;
+
+  const logOut = () => {
+    updateUser({ token: "", username: "", email: "", id: null });
+  };
+
   return (
-    <div className={style.main}>
+    <div className={style.main} onClick={logOut}>
       <img src={LogoutIcon} className={style.resizeIcon} />
       <h3 className={style.h3}>{APP_MESSAGE.logoutLabel}</h3>
     </div>
